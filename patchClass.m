e0eda8172642f13552fa9bd538937c343fbdc076
kevin bradley <postmansyndrome@gmail.com>
2011-10-17 15:03:20 -0700

    fixed sandbox patches

--- patchClass.m
+++ patchClass.m
@@ -18,14 +18,14 @@
 		myData = previousData;
 	else
 		myData = [[NSMutableData alloc] initWithContentsOfMappedFile:inputFile];
-		//NSString *newFile = [inputFile stringByAppendingPathExtension:@"patched"];
+	//NSString *newFile = [inputFile stringByAppendingPathExtension:@"patched"];
 	NSData *dataToFind = [NSData dataWithBytes:oldBytes length:dataLength];
 	NSData *patchData =  [NSData dataWithBytes:newBytes length:dataLength];
 	NSRange searchRange = NSMakeRange(0, myData.length);
 	NSRange dataRange = [myData rangeOfData:dataToFind options:0 range:searchRange];
 	[myData replaceBytesInRange: dataRange withBytes: [patchData bytes]];
 	return [myData autorelease];
-		//[myData writeToFile:newFile atomically:YES];
+	//[myData writeToFile:newFile atomically:YES];
 }
 /*
  
@@ -39,12 +39,47 @@
  
  */
 
+
+/*
+ 
+ credit: msft_guy
+ 
+ potential kernel patches to add
+ 
+ "patternBytes": "00 21 7a 69 01 60 11 60 08 46 0d f5 4b 7d bd e8 00 0d",
+ "patchBytes": "00 20",
+ "patchOffset": "8",
+ "secondMatchCorrelation": "0.6331490278244018555",
+ "adjustedSecondMatchCorrelation": "0.5647840592371871082",
+ "comment": "amfi1; Original offset: 5b564c"
+ },
+ //01 08 00 21 B0 45 F0 DB 08 46 BD E8 00 05 F0 BD
+ "patternBytes": "01 08 00 21 b0 45 f0 db 08 46 bd",
+ "patchBytes": "01 20",
+ "patchOffset": "8",
+ "secondMatchCorrelation": "0.7886300086975097656",
+ "adjustedSecondMatchCorrelation": "0.7431759096110975849",
+ "comment": "amfi2; Original offset: 5b6d70"
+ },
+ 
+ {
+ "patternBytes": "48 98 48 b1 7b 48 00 78 10 f0 04 0f 04 d0 79 48 29 46 79 4e",
+ "patchBytes": "0c b1 00 20",
+ "patchOffset": "8",
+ "secondMatchCorrelation": "0.6743081212043762",
+ "adjustedSecondMatchCorrelation": "0.6110447088403179",
+ "comment": "Sandbox patch; Original offset: 54a622"
+ }
+ 
+ 
+ */
+
 + (NSString *)patchKernelFile:(NSString *)inputFile
 {
 	NSMutableData *myData = [[NSMutableData alloc] initWithContentsOfMappedFile:inputFile];
 	NSString *newFile = [inputFile stringByAppendingPathExtension:@"patched"];
 	
-		//FindNPatch(KernelFile, "A2 6A 1B 68 00 2B 04 BF", "A2 6A 01 23 00 2B 04 BF")
+	//FindNPatch(KernelFile, "A2 6A 1B 68 00 2B 04 BF", "A2 6A 01 23 00 2B 04 BF") //code sign
 	
 	NSData *dataToFind = [NSData dataWithBytes:"\xA2\x6A\x1B\x68\x00\x2B\x04\xBF" length:8];
 	NSData *patchData =  [NSData dataWithBytes:"\xA2\x6A\x01\x23\x00\x2B\x04\xBF" length:8];
@@ -53,21 +88,21 @@
 	
 	if (dataRange.location == NSNotFound)
 	{
-		NSLog(@"first kernel patch failed, %@ not found", dataToFind);
+		NSLog(@"codesigning patch failed, %@ not found", dataToFind);
 	}
 	
 	[myData replaceBytesInRange: dataRange withBytes: [patchData bytes]];
-
-		//	NSLog(@"first patch location: %lld", dataRange.location);
+	
+	//	NSLog(@"first patch location: %lld", dataRange.location);
 	
 	if ([[[inputFile lastPathComponent] pathExtension] isEqualToString:@"k66"]) //dont need for appletv
 	{
 		NSLog(@"appletv skip vm_map_enter");
 		
 	} else {
-			//iPod GM 5: 06 00 06 28 04 BF 19 98
+		//iPod GM 5: 06 00 06 28 04 BF 19 98
 		
-			//FindNPatch(KernelFile, "06 00 06 28 04 BF 22 98", "06 00 FF") //vm_map_enter - may be unnessesary
+		//FindNPatch(KernelFile, "06 00 06 28 04 BF 22 98", "06 00 FF") //vm_map_enter - may be unnessesary
 		
 		dataToFind = [NSData dataWithBytes:"\x06\x00\x06\x28\x04\xBF\x19\x98" length:8];
 		patchData =  [NSData dataWithBytes:"\x06\x00\xFF\x28\x04\xBF\x19\x98" length:8];
@@ -77,33 +112,39 @@
 		
 		if (dataRange.location == NSNotFound)
 		{
-			NSLog(@"second kernel patch failed, %@ not found, vm_map_enter", dataToFind);
+			NSLog(@"vm_map_enter kernel patch failed, %@ not found", dataToFind);
 		}
 	}
 	
-		//FindNPatch(KernelFile, "B0 F1 FF 3F DC BF 43 48 5C F7 0A FD", "00 00 00 00 00 00 00 00 00 00 00 00") '' //B0 F1 FF 3F DC BF 43 48 5C F7 E8 FC  instead?? @ offset 000B8C9C
 	
-		//b4			   B0 F1 FF 3F DC BF 43 48 5C F7 E8 FC 
-		//changed again b5 B0 F1 FF 3F DC BF 43 48 5F F7 12 FB
-		//changed again b6 B0 F1 FF 3F DC BF 43 48 5E F7 0E FB
+	//sandbox!?!?!? :D
 	
-		//ipod 5.0 GM B0 F1 FF 3F DC BF 43 48 5E F7 0E FB
-		
 	
-		//dataToFind = [NSData dataWithBytes:"\xB0\xF1\xFF\x3F\xDC\xBF\x43\x48\x5C\xF7\xE8\xFC" length:12]; //b4	
-		//dataToFind = [NSData dataWithBytes:"\xB0\xF1\xFF\x3F\xDC\xBF\x43\x48\x5F\xF7\x12\xFB" length:12]; //b5
-		//dataToFind = [NSData dataWithBytes:"\xB0\xF1\xFF\x3F\xDC\xBF\x43\x48\x5E\xF7\x0E\xFB" length:12]; //b6
-		
-		dataToFind = [NSData dataWithBytes:"\xB0\xF1\xFF\x3F\xDC\xBF\x43\x48" length:8];
-		patchData =  [NSData dataWithBytes:"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" length:12];
-		dataRange = [myData rangeOfData:dataToFind options:0 range:searchRange];
-		//dataRange2 = [myData rangeOfData:[NSData dataWithBytes:"\xB0\xF1\xFF\x3F\xDC\xBF\x43\x48" length:8] options:0 range:searchRange];
-		//adjust data range to actually be 12 rather than 8
-		dataRange.length = 12;
-		[myData replaceBytesInRange: dataRange withBytes: [patchData bytes]];
 	
-		//NSLog(@"third kernel patch location: %lld length: %lld", dataRange.location, dataRange.length);
-		//	NSLog(@"third kernel patch location2: %lld length: %lld", dataRange2.location, dataRange2.length);
+	
+	//FindNPatch(KernelFile, "B0 F1 FF 3F DC BF 43 48 5C F7 0A FD", "00 00 00 00 00 00 00 00 00 00 00 00") '' //B0 F1 FF 3F DC BF 43 48 5C F7 E8 FC  instead?? @ offset 000B8C9C
+	
+	//b4			   B0 F1 FF 3F DC BF 43 48 5C F7 E8 FC 
+	//changed again b5 B0 F1 FF 3F DC BF 43 48 5F F7 12 FB
+	//changed again b6 B0 F1 FF 3F DC BF 43 48 5E F7 0E FB
+	
+	//ipod 5.0 GM B0 F1 FF 3F DC BF 43 48 5E F7 0E FB
+	
+	
+	//dataToFind = [NSData dataWithBytes:"\xB0\xF1\xFF\x3F\xDC\xBF\x43\x48\x5C\xF7\xE8\xFC" length:12]; //b4	
+	//dataToFind = [NSData dataWithBytes:"\xB0\xF1\xFF\x3F\xDC\xBF\x43\x48\x5F\xF7\x12\xFB" length:12]; //b5
+	//dataToFind = [NSData dataWithBytes:"\xB0\xF1\xFF\x3F\xDC\xBF\x43\x48\x5E\xF7\x0E\xFB" length:12]; //b6
+	
+	dataToFind = [NSData dataWithBytes:"\xB0\xF1\xFF\x3F\xDC\xBF\x43\x48" length:8];
+	patchData =  [NSData dataWithBytes:"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" length:12];
+	dataRange = [myData rangeOfData:dataToFind options:0 range:searchRange];
+	//dataRange2 = [myData rangeOfData:[NSData dataWithBytes:"\xB0\xF1\xFF\x3F\xDC\xBF\x43\x48" length:8] options:0 range:searchRange];
+	//adjust data range to actually be 12 rather than 8
+	dataRange.length = 12;
+	[myData replaceBytesInRange: dataRange withBytes: [patchData bytes]];
+	
+	//NSLog(@"third kernel patch location: %lld length: %lld", dataRange.location, dataRange.length);
+	//	NSLog(@"third kernel patch location2: %lld length: %lld", dataRange2.location, dataRange2.length);
 	
 	if (dataRange.location == NSNotFound)
 	{
@@ -111,7 +152,7 @@
 	}
 	
 	
-		//FindNPatch(KernelFile, "00 21 B0 45 F0 DB 08 46 BD E8 00 05", "00 21 B0 45 F0 DB 01 20")
+	//FindNPatch(KernelFile, "00 21 B0 45 F0 DB 08 46 BD E8 00 05", "00 21 B0 45 F0 DB 01 20")
 	
 	dataToFind = [NSData dataWithBytes:"\x00\x21\xB0\x45\xF0\xDB\x08\x46\xBD\xE8\x00\x05" length:12];
 	patchData =  [NSData dataWithBytes:"\x00\x21\xB0\x45\xF0\xDB\x01\x20\xBD\xE8\x00\x05" length:12];
@@ -123,19 +164,59 @@
 		NSLog(@"fourth kernel patch failed, %@ not found", dataToFind);
 	}
 	
-		//FindNPatch(KernelFile, "00 78 10 F0 04 0F 04 D0", "00 78 01 23 01 23")
+	/*
+	 
+	 { //48 98 48 B1 7B 48 00 78 0C B1 00 20 04 D0 
+	 "patternBytes": "48 98 48 b1 7b 48 00 78 10 f0 04 0f 04 d0 79 48 29 46 79 4e",
+	 "patchBytes": "0c b1 00 20",
+	 "patchOffset": "8",
+	 "secondMatchCorrelation": "0.6743081212043762",
+	 "adjustedSecondMatchCorrelation": "0.6110447088403179",
+	 "comment": "Sandbox patch; current offset: 0x0048161E"
+	 }
+	 
+	 
+	 */
 	
-	dataToFind = [NSData dataWithBytes:"\x00\x78\x10\xF0\x04\x0F\x04\xD0" length:8];
-	patchData =  [NSData dataWithBytes:"\x00\x78\x01\x23\x04\x0F\x04\xD0" length:8];
+	
+	dataToFind = [NSData dataWithBytes:"\x48\x98\x48\xb1\x7b\x48\x00\x78\x10\xf0\x04\x0f\x04\xd0\x79\x48" length:16];
+	patchData =  [NSData dataWithBytes:"\x48\x98\x48\xb1\x7b\x48\x00\x78\x0c\xb1\x00\x20\x04\xd0\x79\x48" length:16];
 	dataRange = [myData rangeOfData:dataToFind options:0 range:searchRange];
 	[myData replaceBytesInRange: dataRange withBytes: [patchData bytes]];
 	
 	if (dataRange.location == NSNotFound)
 	{
-		NSLog(@"fifth kernel patch failed, %@ not found", dataToFind);
+		NSLog(@"sandbox patch failed, %@ not found", dataToFind);
 	}
 	
-		//FindNPatch(KernelFile, "FF 31 A7 F1 18 04 08 46 A5 46 BD E8 00 0D F0 BD", "FF 31 A7 F1 18 04 00 20")
+	
+	/*
+	 
+	 //00 21 7A 69 01 60 11 60 08 46 0D F5 4B 7D BD E8 00 0D 
+	 //
+	 
+	 "patternBytes": "00 21 7a 69 01 60 11 60 08 46 0d f5 4b 7d bd e8 00 0d",
+	 "patchBytes": "00 20",
+	 "patchOffset": "8",
+	 "secondMatchCorrelation": "0.6331490278244018555",
+	 "adjustedSecondMatchCorrelation": "0.5647840592371871082",
+	 "comment": "amfi1; Original offset: 5b564c" //0x004685E4
+	 },
+	 
+	 
+	 */
+	
+	dataToFind = [NSData dataWithBytes:"\x00\x21\x7A\x69\x01\x60\x11\x60\x08\x46\x0D\xF5\x4B\x7D\xBD\xE8" length:16];
+	patchData =  [NSData dataWithBytes:"\x00\x21\x7A\x69\x01\x60\x11\x60\x00\x20\x0D\xF5\x4B\x7D\xBD\xE8" length:16];
+	dataRange = [myData rangeOfData:dataToFind options:0 range:searchRange];
+	[myData replaceBytesInRange: dataRange withBytes: [patchData bytes]];
+	
+	if (dataRange.location == NSNotFound)
+	{
+		NSLog(@"amfi1 patch failed, %@ not found", dataToFind);
+	}
+	
+	//FindNPatch(KernelFile, "FF 31 A7 F1 18 04 08 46 A5 46 BD E8 00 0D F0 BD", "FF 31 A7 F1 18 04 00 20")
 	
 	dataToFind = [NSData dataWithBytes:"\xFF\x31\xA7\xF1\x18\x04\x08\x46\xA5\x46\xBD\xE8\x00\x0D\xF0\xBD" length:16];
 	patchData =  [NSData dataWithBytes:"\xFF\x31\xA7\xF1\x18\x04\x00\x20\xA5\x46\xBD\xE8\x00\x0D\xF0\xBD" length:16];
@@ -144,12 +225,20 @@
 	
 	if (dataRange.location == NSNotFound)
 	{
-		NSLog(@"sixth kernel patch failed, %@ not found", dataToFind);
+		NSLog(@"amfi2 kernel patch failed, %@ not found", dataToFind);
 	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	[myData writeToFile:newFile atomically:YES];
 	
-		//create patch
+	//create patch
 	
 	return [patchClass createBSDiffFromOriginal:inputFile newFile:newFile];
 	
@@ -160,20 +249,20 @@
 	NSMutableData *myData = [[NSMutableData alloc] initWithContentsOfMappedFile:inputFile];
 	NSString *newFile = [inputFile stringByAppendingPathExtension:@"patched"];
 	
-		//PROD 03 93 FF F7 2F FE 00 28
+	//PROD 03 93 FF F7 2F FE 00 28
 	
 	NSData *dataToFind = [NSData dataWithBytes:"\x03\x93\xFF\xF7\x2F\xFE\x00\x28" length:8];
 	NSData *patchData =  [NSData dataWithBytes:"\x03\x93\x00\x20\x00\x20\x00\x28" length:8];
 	NSRange searchRange = NSMakeRange(0, myData.length);
 	NSRange dataRange = [myData rangeOfData:dataToFind options:0 range:searchRange];
 	[myData replaceBytesInRange: dataRange withBytes: [patchData bytes]];
-
+	
 	if (dataRange.location == NSNotFound)
 	{
 		NSLog(@"PROD patch failed, %@ not found", patchData);
 	}
 	
-		//SDOM 03 93 FF F7 42 FE 00 28
+	//SDOM 03 93 FF F7 42 FE 00 28
 	
 	dataToFind = [NSData dataWithBytes:"\x03\x93\xFF\xF7\x42\xFE\x00\x28" length:8];
 	patchData =  [NSData dataWithBytes:"\x03\x93\x00\x20\x00\x20\x00\x28" length:8];
@@ -187,7 +276,7 @@
 	
 	
 	
-		//CHIP 03 93 FF F7 10 FE 00 28
+	//CHIP 03 93 FF F7 10 FE 00 28
 	
 	dataToFind = [NSData dataWithBytes:"\x03\x93\xFF\xF7\x10\xFE\x00\x28" length:8];
 	patchData =  [NSData dataWithBytes:"\x03\x93\x00\x20\x00\x20\x00\x28" length:8];
@@ -202,7 +291,7 @@
 	
 	
 	
-		//TYPE 03 93 FF F7 FE FD 00 28
+	//TYPE 03 93 FF F7 FE FD 00 28
 	
 	dataToFind = [NSData dataWithBytes:"\x03\x93\xFF\xF7\xFE\xFD\x00\x28" length:8];
 	patchData =  [NSData dataWithBytes:"\x03\x93\x00\x20\x00\x20\x00\x28" length:8];
@@ -215,7 +304,7 @@
 		NSLog(@"TYPE patch failed, %@ not found", patchData);
 	}
 	
-		//SEPO 03 95 FF F7 E8 FD 00 28
+	//SEPO 03 95 FF F7 E8 FD 00 28
 	
 	dataToFind = [NSData dataWithBytes:"\x03\x95\xFF\xF7\xE8\xFD\x00\x28" length:8];
 	patchData =  [NSData dataWithBytes:"\x03\x95\x00\x20\x00\x20\x00\x28" length:8];
@@ -228,7 +317,7 @@
 	}
 	
 	
-		//CEPO 03 95 FF F7 D6 FD 00 28
+	//CEPO 03 95 FF F7 D6 FD 00 28
 	
 	dataToFind = [NSData dataWithBytes:"\x03\x95\xFF\xF7\xD6\xFD\x00\x28" length:8];
 	patchData =  [NSData dataWithBytes:"\x03\x95\x00\x20\x00\x20\x00\x28" length:8];
@@ -241,7 +330,7 @@
 	}
 	
 	
-		//BORD 03 94 FF F7 C4 FD 00 28
+	//BORD 03 94 FF F7 C4 FD 00 28
 	
 	dataToFind = [NSData dataWithBytes:"\x03\x94\xFF\xF7\xC4\xFD\x00\x28" length:8];
 	patchData =  [NSData dataWithBytes:"\x03\x94\x00\x20\x00\x20\x00\x28" length:8];
@@ -255,7 +344,7 @@
 		NSLog(@"BORD patch failed, %@ not found", patchData);
 	}
 	
-		//ECID 03 94 FF F7 AF FD 00 28
+	//ECID 03 94 FF F7 AF FD 00 28
 	
 	dataToFind = [NSData dataWithBytes:"\x03\x94\xFF\xF7\xAF\xFD\x00\x28" length:8];
 	patchData =  [NSData dataWithBytes:"\x03\x94\x00\x20\x00\x20\x00\x28" length:8];
@@ -267,9 +356,9 @@
 		NSLog(@"ECID patch failed, %@ not found", patchData);
 	}
 	
-		//SHSH [patchClass patchFile:one bytes:"\x4F\xF0\xFF\x30\x01\xE0\x4F\xF0\xFF\x30" withBytes:"\x00\x20\x00\x20\x01\xE0\x00\x20\x00\x20" length:10]; //SHSH
+	//SHSH [patchClass patchFile:one bytes:"\x4F\xF0\xFF\x30\x01\xE0\x4F\xF0\xFF\x30" withBytes:"\x00\x20\x00\x20\x01\xE0\x00\x20\x00\x20" length:10]; //SHSH
 	
-		//4F F0 FF 30 01 E0 4F F0 FF 30
+	//4F F0 FF 30 01 E0 4F F0 FF 30
 	
 	dataToFind = [NSData dataWithBytes:"\x4F\xF0\xFF\x30\x01\xE0\x4F\xF0\xFF\x30" length:10];
 	patchData =  [NSData dataWithBytes:"\x00\x20\x00\x20\x01\xE0\x00\x20\x00\x20" length:10];
@@ -302,7 +391,7 @@
 #define SIG_RANGE NSMakeRange(0x2BEF6, 20)
 
 
-	//179958
+//179958
 
 + (NSString *)patchASRFile:(NSString *)inputFile
 {
@@ -314,7 +403,7 @@
 	 
 	 
 	 */
-
+	
 	NSString *newFile = [inputFile stringByAppendingString:@"_patched"];
 	NSMutableData *myData = [[NSMutableData alloc] initWithContentsOfMappedFile:inputFile];
 	char *jump_PROPER = "\x4D\xF2";
@@ -331,7 +420,7 @@
 		[myData replaceBytesInRange:JUMP_RANGE withBytes:"\xF5\xE7"];
 		NSLog(@"asr jump patched successfully");
 	}
-
+	
 	if ([sig_OGData isEqualToData:sig_ProperData])
 	{
 		[myData replaceBytesInRange:SIG_RANGE withBytes:"\xF6\x93\xB6\xC7\x4F\x33\xE1\xBA\xE2\x9D\xBC\x30\x1B\x81\xDE\xD2\x0D\x65\x10\x76"];
@@ -344,7 +433,7 @@
 	[myData release];
 	if (writeFile)
 	{
-			
+		
 		NSLog(@"success!");
 		return [patchClass createBSDiffFromOriginal:inputFile newFile:newFile];
 		
